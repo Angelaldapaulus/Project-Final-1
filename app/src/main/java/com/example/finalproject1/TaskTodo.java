6681a62ab93e6aa962c8376c36bb65d68a3053b2
AngelAlda <99969516+Angelaldapaulus@users.noreply.github.com>
2022-04-28 20:07:08 +0700

    ProjectFinal1

---
+++ app/src/main/java/com/example/finalproject1/TaskTodo.java
@@ -0,0 +1,36 @@
+package com.example.finalproject1;
+
+public class TaskTodo {
+    int id;
+    String taskTodo;
+
+    public TaskTodo() {
+        super();
+    }
+
+    public TaskTodo(int id, String taskTodo) {
+        this.id = id;
+        this.taskTodo = taskTodo;
+    }
+
+    public TaskTodo(String taskTodo) {
+        this.taskTodo = taskTodo;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTaskTodo(String taskTodo) {
+        this.taskTodo = taskTodo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTaskTodo() {
+        return taskTodo;
+    }
+}
